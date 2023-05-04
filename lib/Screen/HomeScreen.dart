f6dd89cef7e59578b04b52cd601688951675be57
Asad <asadkambo2021@gmail.com>
2023-05-05 00:24:04 +0500

    Add Video Indicator

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -16,32 +16,59 @@ class _HomeScreenState extends State<HomeScreen> {
     _controller = VideoPlayerController.asset("assets/videos/asad.mp4",
         videoPlayerOptions: VideoPlayerOptions())
       ..initialize().then((value) {
-        setState(() {
-          VideoProgressIndicator(
-            _controller,
-            allowScrubbing: true,
-            colors: VideoProgressColors(
-                backgroundColor: Colors.red,
-                bufferedColor: Colors.black,
-                playedColor: Colors.blueAccent),
-          );
-        });
+        setState(() {});
       });
     super.initState();
     _controller.play();
+    _controller.setVolume(100);
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        width: 300,
-        height: 300,
-        child: _controller.value.isInitialized
-            ? VideoPlayer(_controller)
-            : Container(),
-      ),
+      body: Stack(children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                _controller.value.isPlaying
+                    ? _controller.pause()
+                    : _controller.play();
+              },
+              child: Container(
+                child: _controller.value.isInitialized
+                    ? AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        child: VideoPlayer(_controller),
+                      )
+                    : Container(),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              width: MediaQuery.of(context).size.width,
+              child: VideoProgressIndicator(
+                _controller,
+                allowScrubbing: true,
+                colors: VideoProgressColors(
+                    backgroundColor: Colors.black,
+                    bufferedColor: Colors.grey,
+                    playedColor: Colors.red),
+              ),
+            )
+          ],
+        ),
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _controller.value.isPlaying
